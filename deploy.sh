07c44be97041fab0e5ec0dfe39b8f03724c01be1
paulfantom <pawel@krupa.net.pl>
2018-03-31 15:37:36 +0200

    add Ansible Run Analysis

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 export ANSIBLE_FORCE_COLOR=true
+export ANSIBLE_CALLBACK_PLUGINS="$(python -c 'import os,ara; print(os.path.dirname(ara.__file__))')/plugins/callbacks"
 
 echo "Creating Vault-file: /tmp/vault"
 /bin/cat <<EOM >/tmp/vault
@@ -9,6 +10,11 @@ EOM
 
 ansible-playbook --vault-id /tmp/vault extras.yml site.yml
 EXIT_CODE=$?
+
+# Copy ARA static files
+ara generate html ara
+tar cvf ara.tgz ara
+ansible --vault-id /tmp/vault -m unarchive -a "src=ara.tgz dest=/var/www/" web
 
 shred /tmp/vault
 
