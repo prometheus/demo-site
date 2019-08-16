8acd6855eb1cdc899ff3f3bab056c8f4610052a1
paulfantom <pawel@krupa.net.pl>
2019-08-16 12:48:17 +0200

    remove ARA

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 export ANSIBLE_FORCE_COLOR=true
-export ANSIBLE_CALLBACK_PLUGINS="$(python -c 'import os,ara; print(os.path.dirname(ara.__file__))')/plugins/callbacks"
 
 echo "Creating Vault-file: /tmp/vault"
 /bin/cat <<EOM >/tmp/vault
@@ -10,11 +9,6 @@ EOM
 
 ansible-playbook --vault-id /tmp/vault extras.yml site.yml
 EXIT_CODE=$?
-
-# Copy ARA static files
-ara generate html ara
-tar cf ara.tgz ara
-ansible --vault-id /tmp/vault -m unarchive -a "src=ara.tgz dest=/var/www/demo/" web
 
 shred /tmp/vault
 
