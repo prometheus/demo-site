96f6004533a545758c0ae49079d053320c201d9a
paulfantom <pawel@krupa.net.pl>
2020-01-10 23:41:42 +0100

    switch to circleci
    Signed-off-by: paulfantom <pawel@krupa.net.pl>

--- deploy.sh
+++ deploy.sh
@@ -2,12 +2,14 @@
 
 export ANSIBLE_FORCE_COLOR=true
 
+ansible-galaxy install -f -r roles/requirements.yml
+
 echo "Creating Vault-file: /tmp/vault"
 /bin/cat <<EOM >/tmp/vault
 ${ANSIBLE_VAULT_PASSWORD}
 EOM
 
-ansible-playbook --vault-id /tmp/vault extras.yml site.yml
+ansible-playbook --vault-id /tmp/vault site.yml
 EXIT_CODE=$?
 
 shred /tmp/vault
